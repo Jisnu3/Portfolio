15ac17a0998a0b8ce9fe400f92d279776b16e923
Jisnu Hazra <jisnuhazra@gmail.com>
2025-04-15 16:48:11 +0530

    Deleted the file filename

--- jisnu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var typed = new Typed('.text', {
-        strings: ["Frontend Developer", "Web Developer", "Player"],
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
-});
