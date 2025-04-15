9d7c540ce47d41747d274431679642fb7aad956a
Jisnu Hazra <jisnuhazra@gmail.com>
2025-04-15 15:45:33 +0530

    Update website

---
+++ jisnu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.text', {
+        strings: ["Frontend Developer", "Web Developer", "Player"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
